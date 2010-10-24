42452e0c1c9a812bd03e79563488ffaccaf4c821
stephan.com <stephan@stephan.com>
2010-10-24 07:29:58 -0700

    some additional cleanup to my tastes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 SoundcloudSocialDownload::Application.routes.draw do
-  resources :users
-  resource :session
-  resources :posts
+  resources :users, :only => [:index, :create]
   match '/export' => 'users#index', :as => :export
+
+  resource :session, :only => [:new, :destroy]
   match '/activate' => 'sessions#new', :as => :activate, :service => 'soundcloud'
   match '/login/:service' => 'sessions#new', :as => :login
   match '/logout' => 'sessions#destroy', :as => :logout
-  match '/download' => 'posts#download', :as => :download
   match '/oauth_callback/:service' => 'sessions#oauth_callback', :as => :oauth_callback
+
+  resources :posts, :only => [:new, :create]
+  match '/download' => 'posts#download', :as => :download
   match '/' => 'posts#new', :as => :root
-  match '/:controller(/:action(/:id))'
 end
