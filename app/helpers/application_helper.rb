c4debf5df2565ba3c44615b995075a70d86b2f73
stephan.com <stephan@stephan.com>
2010-10-26 01:52:08 -0700

    iphone/ipad visitors get image instead of player; email field has html5 email type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def is_ios?
+    request.env["HTTP_USER_AGENT"] && 
+      (request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/] || 
+       request.env["HTTP_USER_AGENT"].include?('iPhone') || 
+       request.env["HTTP_USER_AGENT"].include?('iPad'))
+  end
 end
